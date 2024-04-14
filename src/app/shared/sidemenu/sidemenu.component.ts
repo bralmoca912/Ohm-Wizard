5847fed27a38640d3aa13eb890d9c57851800f62
BrDev <bmontero912@gmail.com>
2024-04-14 13:32:51 -0500

    Sidemenu terminado

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { routes } from '../../app.routes';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidemenu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './sidemenu.component.html',
   styles: ``
 })
 export class SidemenuComponent {
 
+  public menuItems = routes
+    .map((route) => route.children ?? [])
+    .flat()
+    .filter((route) => route && route.path)
+    .filter((route) => !route.path?.includes(':'));
+
+  constructor() { }
 }
