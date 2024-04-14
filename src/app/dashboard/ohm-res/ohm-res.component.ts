0c8fec226f7c720c1ed1c86d47bf92cb56cb940d
BrDev <bmontero912@gmail.com>
2024-04-14 14:14:43 -0500

    Ohm-res cálculo del potencia parte 1

--- src/app/dashboard/ohm-res/ohm-res.component.ts
+++ src/app/dashboard/ohm-res/ohm-res.component.ts
@@ -9,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export default class OhmResComponent {
 
+  resistencia: number = 0;
+  voltaje: number = 0;
+  corriente: number = 0;
+  potencia: number = 0;
+
+  clacpotencia() {
+    this.potencia = this.voltaje * this.corriente;
+  }
+
 }
