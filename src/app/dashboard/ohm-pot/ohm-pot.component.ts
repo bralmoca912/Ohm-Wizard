2ad7c6e52f96606a64bd087de272b9b68420c878
BrDev <bmontero912@gmail.com>
2024-04-15 18:28:50 -0500

    Component title 1 para todos

--- src/app/dashboard/ohm-pot/ohm-pot.component.ts
+++ src/app/dashboard/ohm-pot/ohm-pot.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TitleComponent } from '../../shared/title/title.component';
 
 @Component({
   selector: 'app-ohm-pot',
   standalone: true,
-  imports: [],
+  imports: [TitleComponent],
   templateUrl: './ohm-pot.component.html',
   styles: ``
 })
--- src/app/dashboard/opams/opams.component.ts
+++ src/app/dashboard/opams/opams.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TitleComponent } from '../../shared/title/title.component';
 
 @Component({
   selector: 'app-opams',
   standalone: true,
-  imports: [],
+  imports: [TitleComponent],
   templateUrl: './opams.component.html',
   styles: ``
 })
