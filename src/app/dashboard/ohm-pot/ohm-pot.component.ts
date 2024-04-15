344b2508ef6cccf9ef72ea7fcb6bc8176aa6a45d
BrDev <bmontero912@gmail.com>
2024-04-15 18:31:06 -0500

    Calculadora de potencia funcional

--- src/app/dashboard/ohm-pot/ohm-pot.component.ts
+++ src/app/dashboard/ohm-pot/ohm-pot.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { TitleComponent } from '../../shared/title/title.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-ohm-pot',
   standalone: true,
-  imports: [TitleComponent],
+  imports: [TitleComponent, FormsModule],
   templateUrl: './ohm-pot.component.html',
   styles: ``
 })
 export default class OhmPotComponent {
 
+  voltaje: number = 0;
+  corriente: number = 0;
+  potencia: number = 0;
+
+  clacpotencia(): void {
+    // title = 'Calculadoras de la Ley de Ohm';
+    this.potencia = this.voltaje * this.corriente;
+  }
 }
