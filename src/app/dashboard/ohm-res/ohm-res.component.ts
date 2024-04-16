41e5e520527ecdceb0909e0fe060e4afdf237537
BrDev <bmontero912@gmail.com>
2024-04-15 20:01:19 -0500

    Calculadoras de ohm-res finalizadas

--- src/app/dashboard/ohm-res/ohm-res.component.ts
+++ src/app/dashboard/ohm-res/ohm-res.component.ts
@@ -11,13 +11,28 @@ import { TitleComponent } from '../../shared/title/title.component';
 })
 export default class OhmResComponent {
 
-  voltaje: number = 0;
-  corriente: number = 0;
-  resistencia: number = 0;
-
-  clacresistencia(): void {
+  voltaje_r: number = 0;
+  corriente_r: number = 0;
+  resistencia_r: number = 0;
+  calcresistencia(): void {
     // title = 'Calculadoras de la Ley de Ohm';
-    this.resistencia = parseFloat((this.voltaje / this.corriente).toFixed(2));
+    this.resistencia_r = parseFloat((this.voltaje_r / this.corriente_r).toFixed(2));
+  }
+
+  voltaje_v: number = 0;
+  corriente_v: number = 0;
+  resistencia_v: number = 0;
+  calcvoltaje(): void {
+    // title = 'Calculadoras de la Ley de Ohm';
+    this.voltaje_v = parseFloat((this.resistencia_v * this.corriente_v).toFixed(2));
+  }
+
+  voltaje_c: number = 0;
+  corriente_c: number = 0;
+  resistencia_c: number = 0;
+  calccorriente(): void {
+    // title = 'Calculadoras de la Ley de Ohm';
+    this.corriente_c = parseFloat((this.voltaje_c / this.resistencia_c).toFixed(2));
   }
 
 }
