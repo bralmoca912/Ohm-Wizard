f735e6364d3c3ba5dfe8977651c975c727b92458
BrDev <bmontero912@gmail.com>
2024-04-17 09:23:30 -0500

    Commit 1 del 17/04/2024

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,12 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
+    screens:{ // Cambiamos el tamaño de las pantallas
+      'sm': '720px',
+      'md': '930px',
+      'lg': '1230px',
+      'xl': '1536px',
+    },
     extend: {},
   },
   plugins: [],
