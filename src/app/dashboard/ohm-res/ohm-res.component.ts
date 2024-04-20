57dffdd0f524f4176ddf27ce0be9dd9fb7d4e49f
BrDev <bmontero912@gmail.com>
2024-04-20 01:02:17 -0500

    ohm-res El cero en la entrada ha sido desaparecido al cargar la página

--- src/app/dashboard/ohm-res/ohm-res.component.ts
+++ src/app/dashboard/ohm-res/ohm-res.component.ts
@@ -11,52 +11,52 @@ import { TitleComponent } from '../../shared/title/title.component';
 })
 export default class OhmResComponent {
 
-  voltaje_r: number = 0;
-  corriente_r: number = 0;
-  resistencia_r: number = 0;
+  voltaje_r: number | undefined = undefined;
+  corriente_r: number | undefined = undefined;
+  resistencia_r: number | undefined = undefined;
   calcresistencia(): void {
     // title = 'Calculadoras de la Ley de Ohm';
-    this.resistencia_r = parseFloat((this.voltaje_r / this.corriente_r).toFixed(2));
+    this.resistencia_r = parseFloat((this.voltaje_r! / this.corriente_r!).toFixed(2));
   }
 
-  voltaje_v: number = 0;
-  corriente_v: number = 0;
-  resistencia_v: number = 0;
+  voltaje_v: number | undefined = undefined;
+  corriente_v: number | undefined = undefined;
+  resistencia_v: number | undefined = undefined;
   calcvoltaje(): void {
     // title = 'Calculadoras de la Ley de Ohm';
-    this.voltaje_v = parseFloat((this.resistencia_v * this.corriente_v).toFixed(2));
+    this.voltaje_v = parseFloat((this.resistencia_v! * this.corriente_v!).toFixed(2));
   }
 
-  voltaje_c: number = 0;
-  corriente_c: number = 0;
-  resistencia_c: number = 0;
+  voltaje_c: number | undefined = undefined;
+  corriente_c: number | undefined = undefined;
+  resistencia_c: number | undefined = undefined;
   calccorriente(): void {
     // title = 'Calculadoras de la Ley de Ohm';
-    this.corriente_c = parseFloat((this.voltaje_c / this.resistencia_c).toFixed(2));
+    this.corriente_c = parseFloat((this.voltaje_c! / this.resistencia_c!).toFixed(2));
   }
 
-  voltaje_p1: number = 0;
-  corriente_p1: number = 0;
-  potencia_p1: number = 0;
+  voltaje_p1: number | undefined = undefined;
+  corriente_p1: number | undefined = undefined;
+  potencia_p1: number | undefined = undefined;
   calcpotencia_p1(): void {
     // title = 'Calculadoras de la Ley de Ohm';
-    this.potencia_p1 = parseFloat((this.voltaje_p1 * this.corriente_p1).toFixed(2));
+    this.potencia_p1 = parseFloat((this.voltaje_p1! * this.corriente_p1!).toFixed(2));
   }
 
-  resistencia_p2: number = 0;
-  corriente_p2: number = 0;
-  potencia_p2: number = 0;
+  resistencia_p2: number | undefined = undefined;
+  corriente_p2: number | undefined = undefined;
+  potencia_p2: number | undefined = undefined;
   calcpotencia_p2(): void {
     // title = 'Calculadoras de la Ley de Ohm';
-    this.potencia_p2 = parseFloat((this.corriente_p2 * this.corriente_p2 * this.resistencia_p2).toFixed(2));
+    this.potencia_p2 = parseFloat((this.corriente_p2! * this.corriente_p2! * this.resistencia_p2!).toFixed(2));
   }
 
-  voltaje_p3: number = 0;
-  resistencia_p3: number = 0;
-  potencia_p3: number = 0;
+  voltaje_p3: number | undefined = undefined;
+  resistencia_p3: number | undefined = undefined;
+  potencia_p3: number | undefined = undefined;
   calcpotencia_p3(): void {
     // title = 'Calculadoras de la Ley de Ohm';
-    this.potencia_p3 = parseFloat((this.voltaje_p3 * this.voltaje_p3 / this.resistencia_p3).toFixed(2));
+    this.potencia_p3 = parseFloat((this.voltaje_p3! * this.voltaje_p3! / this.resistencia_p3!).toFixed(2));
   }
 
 }
